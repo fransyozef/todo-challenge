83ac1a6dd988cf82969a4172d881c762c65643a3
Fransjo Leihitu <info@coreelements.nl>
2019-08-20 11:55:36 +0200

    added nestjs cors

--- backend/nestjs/todo/src/main.ts
+++ backend/nestjs/todo/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// tslint:disable-next-line:no-console
-console.log("env" , process.env["PRISMA_ENDPOINT"]);
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule , { cors: true });
   await app.listen(3000);
 }
 bootstrap();

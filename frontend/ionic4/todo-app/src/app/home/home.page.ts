0afc58df072fa34c2b5d95fe4bc3e32b77a748f7
Fransjo Leihitu <info@coreelements.nl>
2019-11-19 22:06:20 +0100

    IONIC // add version

--- frontend/ionic4/todo-app/src/app/home/home.page.ts
+++ frontend/ionic4/todo-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PACKAGE_VERSION } from '../../../version';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  version  = PACKAGE_VERSION;
   constructor( ) { }
   ngOnInit() { }
 }
